6cc1bdb2962868316a647425faa12672574fc2ff
jaehoon7368 <109495226+jaehoon7368@users.noreply.github.com>
2022-09-27 18:02:19 +0900

    Update TestBook.java

--- 22.09.26/book/controller/TestBook.java
+++ 22.09.26/book/controller/TestBook.java
@@ -7,8 +7,8 @@ public class TestBook {
 	public static void main(String[] args) {
 		Book b1 = new Book("자바","홍길동",50_000);
 		Book b2 = new Book("자바","홍길동",50_000);
-		System.out.println(b1);
-		System.out.println(b2);
+		System.out.println(b1.toString());
+		System.out.println(b2.toString());
 		
 		System.out.println(b1.equals(b2));
 		
